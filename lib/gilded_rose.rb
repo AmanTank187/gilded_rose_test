b00c0bfa6e779fe5db6f72ae1d9329b2868d3a48
Aman Tank <amantank187@gmail.com>
2021-02-10 15:42:01 +0000

    Created constant variables

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,5 +1,8 @@
 class GildedRose
 
+  MIN_QUALITY = 0 
+  MAX_QUALITY = 50
+
   def initialize(items)
     @items = items
   end
@@ -21,9 +24,8 @@ class GildedRose
 
   def update_normal(item)
     item.sell_in -= 1
-    item.sell_in < 0 ? item.quality -=2 : item.quality -=1
-    item.quality = 0 if item.quality < 0
-          
+    item.sell_in < MIN_QUALITY ? item.quality -=2 : item.quality -=1
+    item.quality = MIN_QUALITY if item.quality < MIN_QUALITY    
   end 
 
   def update_brie(item)
@@ -39,12 +41,11 @@ class GildedRose
         item.quality += 2
      else 
         item.quality += 1
-     end 
-          
-    item.quality = 0 if item.sell_in < 0
-          
-    item.quality = 50 if item.quality > 50
-      
+     end
+     
+     
+    item.quality = MIN_QUALITY if item.sell_in < MIN_QUALITY
+    item.quality = MAX_QUALITY if item.quality > MAX_QUALITY
     end 
   end
 
