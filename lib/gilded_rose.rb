10ec5546435c862b822ca63f00b80c1d5cc411d3
Aman Tank <amantank187@gmail.com>
2021-02-10 16:07:10 +0000

    Took out an IF statement for a Switch case

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,53 +1,65 @@
-class GildedRose
+# frozen_string_literal: true
 
-  MIN_QUALITY = 0 
+class GildedRose
+  MIN_QUALITY = 0
   MAX_QUALITY = 50
 
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name == "Aged Brie"
+      case item.name
+      when 'Aged Brie'
         update_brie(item)
-      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+      when 'Backstage passes to a TAFKAL80ETC concert'
         update_backstage(item)
-      elsif item.name == "Sulfuras, Hand of Ragnaros"
-      else 
+      when 'Sulfuras, Hand of Ragnaros'
+        item.quality = 80
+        item.sell_in = 0
+      else
         update_normal(item)
-      end 
-    end 
-  end 
+      end
+    end
+  end
 
-  private 
+  private
 
   def update_normal(item)
     item.sell_in -= 1
-    item.sell_in < MIN_QUALITY ? item.quality -=2 : item.quality -=1
-    item.quality = MIN_QUALITY if item.quality < MIN_QUALITY    
-  end 
+    item.quality -= item.sell_in < MIN_QUALITY ? 2 : 1
+    quality_restriction(item)
+  end
 
   def update_brie(item)
     item.sell_in -= 1
-    item.quality += 1 if item.quality < 50 
-  end 
+    item.quality += 1
+    quality_restriction(item)
+  end
 
   def update_backstage(item)
     item.sell_in -= 1
-     if item.sell_in < 6
-        item.quality += 3
-     elsif item.sell_in < 11
-        item.quality += 2
-     else 
-        item.quality += 1
-     end
-     
-     
+    item.quality += if item.sell_in < 6
+                      3
+                    elsif item.sell_in < 11
+                      2
+                    else
+                      1
+                    end
+
     item.quality = MIN_QUALITY if item.sell_in < MIN_QUALITY
-    item.quality = MAX_QUALITY if item.quality > MAX_QUALITY
-    end 
+    quality_restriction(item)
   end
+
+  def quality_restriction(item)
+    if item.quality < MIN_QUALITY
+      item.quality = MIN_QUALITY
+    elsif item.quality > MAX_QUALITY
+      item.quality = MAX_QUALITY
+    end
+  end
+end
 
 class Item
   attr_accessor :name, :sell_in, :quality
@@ -58,7 +70,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,77 +1,77 @@
+# frozen_string_literal: true
+
 require 'gilded_rose'
 
 describe GildedRose do
-
-  describe "#update_quality" do
-    it "Quality and Sellin of regular item is decreased by 1" do
-      items = [Item.new("foo", 5, 10)]
-      GildedRose.new(items).update_quality()
+  describe '#update_quality' do
+    it 'Quality and Sellin of regular item is decreased by 1' do
+      items = [Item.new('foo', 5, 10)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 9
       expect(items[0].sell_in).to eq 4
     end
 
-    it "Regular item decreases by two after sell by date" do
-      items = [Item.new("foo", 0, 10)]
-      GildedRose.new(items).update_quality()
+    it 'Regular item decreases by two after sell by date' do
+      items = [Item.new('foo', 0, 10)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 8
-      expect(items[0].sell_in).to eq -1
+      expect(items[0].sell_in).to eq(-1)
     end
 
-    it "Stops at 0 even if it should -2 from the quality" do
-      items = [Item.new("foo", 0, 1)]
-      GildedRose.new(items).update_quality()
+    it 'Stops at 0 even if it should -2 from the quality' do
+      items = [Item.new('foo', 0, 1)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 0
-      expect(items[0].sell_in).to eq -1
+      expect(items[0].sell_in).to eq(-1)
     end
 
-    it "Item cannot go below 0 quality" do
-      items = [Item.new("foo", 10, 0)]
-      GildedRose.new(items).update_quality()
+    it 'Item cannot go below 0 quality' do
+      items = [Item.new('foo', 10, 0)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 0
       expect(items[0].sell_in).to eq 9
     end
 
-
-    it "Aged brie should increase in quality as sell_in decreasing" do
-      items = [Item.new("Aged Brie", 5, 10)]
-      GildedRose.new(items).update_quality()
+    it 'Aged brie should increase in quality as sell_in decreasing' do
+      items = [Item.new('Aged Brie', 5, 10)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 11
       expect(items[0].sell_in).to eq 4
-     end 
-    
-    it "Item like Aged brie cannot go above the quality of 50" do
-      items = [Item.new("Aged Brie", 5, 50)]
-      GildedRose.new(items).update_quality()
+    end
+
+    it 'Item like Aged brie cannot go above the quality of 50' do
+      items = [Item.new('Aged Brie', 5, 50)]
+      GildedRose.new(items).update_quality
       expect(items[0].quality).to eq 50
       expect(items[0].sell_in).to eq 4
-    end 
+    end
 
-    it "Backstage passes increase in value +2 if 10 days are left" do
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
-          GildedRose.new(items).update_quality()
-          expect(items[0].quality).to eq 12
-          expect(items[0].sell_in).to eq 9
-        end 
-    
-        it "Backstage passes increase in value +3 if 5 days are left" do
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
-          GildedRose.new(items).update_quality()
-          expect(items[0].quality).to eq 13
-          expect(items[0].sell_in).to eq 4
-        end 
-    
-        it "Backstage passes set to quality of 0 if sell_in is negative" do
-          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", -1, 45)]
-          GildedRose.new(items).update_quality()
-          expect(items[0].quality).to eq 0
-          expect(items[0].sell_in).to eq -2
-        end 
+    it 'Backstage passes increase in value +2 if 10 days are left' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 10, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 12
+      expect(items[0].sell_in).to eq 9
+    end
 
-        it "Checks Sulfuras stays the same" do
-                items = [Item.new("Sulfuras, Hand of Ragnaros", 5, 10)]
-                GildedRose.new(items).update_quality()
-                expect(items[0].quality).to eq 10
-                expect(items[0].sell_in).to eq 5
-        end 
-      end 
+    it 'Backstage passes increase in value +3 if 5 days are left' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 10)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 13
+      expect(items[0].sell_in).to eq 4
+    end
+
+    it 'Backstage passes set to quality of 0 if sell_in is negative' do
+      items = [Item.new('Backstage passes to a TAFKAL80ETC concert', -1, 45)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 0
+      expect(items[0].sell_in).to eq(-2)
+    end
+
+    it 'Checks Sulfuras stays the same' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 10, 40)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq 80
+      expect(items[0].sell_in).to eq 0
+    end
+  end
 end
